03f3164fc85fd396517c7a7e94a32f6897b1d4dd
Monu Singh <125896049+monurajputrko@users.noreply.github.com>
2023-12-03 23:56:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
-const colors = require("colors");
+
 require('dotenv').config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log(
-      `Connected to Mongodb Database ${mongoose.connection.host}`.bgMagenta
-        .white
-    );
   } catch (error) {
-    console.log(`MONGO Connect Error ${error}`.bgRed.white);
+    console.log("error in db.js");
   }
 };
 
