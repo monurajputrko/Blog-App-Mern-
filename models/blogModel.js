df7f0621ffee931ffe7658b3624e9e9f672334cc
Monu Singh <125896049+monurajputrko@users.noreply.github.com>
2023-12-04 00:23:45 +0530

    Update blogModel.js

--- models/blogModel.js
+++ models/blogModel.js
@@ -24,7 +24,7 @@ const blogSchema = new mongoose.Schema(
       require: [true, "user id is required"],
     },
   },
-  { timestamps: true }
+  
 );
 
 const blogModel = mongoose.model("Blog", blogSchema);
